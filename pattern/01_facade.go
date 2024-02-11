722bc0a060b938ad2c542d89fedbb34b86aa3896
ArtPr0g <91186039+ArtPr0g@users.noreply.github.com>
2024-02-11 16:26:12 +0300

    Update 01_facade.go

--- pattern/01_facade.go
+++ pattern/01_facade.go
@@ -1,7 +1,102 @@
 package pattern
 
+import "fmt"
 /*
-	Реализовать паттерн «фасад».
-Объяснить применимость паттерна, его плюсы и минусы,а также реальные примеры использования данного примера на практике.
-	https://en.wikipedia.org/wiki/Facade_pattern
+Допустим, у нас есть следующие компоненты банковской системы:
+
+Авторизация пользователя
+Проверка баланса
+Перевод денег
+Создадим интерфейсы для каждого из этих компонентов:
 */
+
+type Authorization interface {
+    Authorize(username string, password string) bool
+}
+
+type AccountBalanceChecker interface {
+    GetAccountBalance(accountNumber string) float64
+}
+
+type MoneyTransferer interface {
+    TransferMoney(senderAccountNumber string, receiverAccountNumber string, amount float64) bool
+}
+// Затем реализуем конкретные классы для каждого из интерфейсов:
+
+go
+Copy code
+type AuthorizationService struct {
+    // реализация интерфейса Authorization
+}
+
+func (a *AuthorizationService) Authorize(username string, password string) bool {
+    // реализация авторизации пользователя
+}
+
+type AccountBalanceCheckerService struct {
+    // реализация интерфейса AccountBalanceChecker
+}
+
+func (a *AccountBalanceCheckerService) GetAccountBalance(accountNumber string) float64 {
+    // реализация получения баланса
+}
+
+type MoneyTransferService struct {
+    // реализация интерфейса MoneyTransferer
+}
+
+func (m *MoneyTransferService) TransferMoney(senderAccountNumber string, receiverAccountNumber string, amount float64) bool {
+    // реализация перевода денег
+}
+// Теперь создадим структуру Фасада, которая будет предоставлять простой интерфейс для работы с банковской системой:
+
+go
+Copy code
+type BankSystemFacade struct {
+    authorization     Authorization
+    balanceChecker    AccountBalanceChecker
+    moneyTransferer   MoneyTransferer
+}
+
+func NewBankSystemFacade() *BankSystemFacade {
+    return &BankSystemFacade{
+        authorization:     &AuthorizationService{},
+        balanceChecker:    &AccountBalanceCheckerService{},
+        moneyTransferer:   &MoneyTransferService{},
+    }
+}
+
+func (f *BankSystemFacade) Login(username string, password string) bool {
+    return f.authorization.Authorize(username, password)
+}
+
+func (f *BankSystemFacade) CheckBalance(accountNumber string) float64 {
+    return f.balanceChecker.GetAccountBalance(accountNumber)
+}
+
+func (f *BankSystemFacade) TransferMoney(senderAccountNumber string, receiverAccountNumber string, amount float64) bool {
+    return f.moneyTransferer.TransferMoney(senderAccountNumber, receiverAccountNumber, amount)
+}
+//Теперь можем использовать Фасад для упрощенного взаимодействия с банковской системой:
+
+go
+Copy code
+func main() {
+    facade := NewBankSystemFacade()
+
+    authorized := facade.Login("username", "password")
+    if authorized {
+        balance := facade.CheckBalance("accountNumber")
+        fmt.Println("Balance:", balance)
+
+        transferred := facade.TransferMoney("senderAccountNumber", "receiverAccountNumber", 100.0)
+        if transferred {
+            fmt.Println("Перевод денег выполнен")
+        } else {
+            fmt.Println("Ошибка перевода денег")
+        }
+    } else {
+        fmt.Println("Ошибка авторизации")
+    }
+}
+//Таким образом, мы использовали паттерн Фасад для создания упрощенного интерфейса для работы с банковской системой, скрывая сложность ее компонентов от клиента.
