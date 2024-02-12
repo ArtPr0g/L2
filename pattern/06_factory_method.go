2f92684bc999dc771e34cca2b8a698870155722a
ArtPr0g <91186039+ArtPr0g@users.noreply.github.com>
2024-02-12 13:56:52 +0300

    Update 06_factory_method.go

--- pattern/06_factory_method.go
+++ pattern/06_factory_method.go
@@ -1,7 +1,54 @@
 package pattern
 
 /*
-	Реализовать паттерн «фабричный метод».
-Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
-	https://en.wikipedia.org/wiki/Factory_method_pattern
+	Данный патерн используется для создания объектов без необходимости указывать конкретный класс объекта, который нужно создать.
 */
+
+import "fmt"
+
+
+type Transport interface {
+    Drive()
+}
+
+// Структура автомобиля
+type Car struct{}
+
+func (c Car) Drive() {
+    fmt.Println("Driving a car 🚗")
+}
+
+// Структура самолета
+type Plane struct{}
+
+func (p Plane) Drive() {
+    fmt.Println("Flying a plane ✈️")
+}
+
+// Фабрика транспорта
+type TransportFactory interface {
+    CreateTransport() Transport
+}
+
+type CarFactory struct{}
+
+func (cf CarFactory) CreateTransport() Transport {
+    return Car{}
+}
+
+// Фабрика для создания самолетов
+type PlaneFactory struct{}
+
+func (pf PlaneFactory) CreateTransport() Transport {
+    return Plane{}
+}
+
+func main() {
+    carFactory := CarFactory{}
+    car := carFactory.CreateTransport()
+    car.Drive()
+
+    planeFactory := PlaneFactory{}
+    plane := planeFactory.CreateTransport()
+    plane.Drive()
+}
